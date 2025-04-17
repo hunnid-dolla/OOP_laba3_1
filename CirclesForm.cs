ca782c820f00723b5f21352c370c88e733eb6c95
hunnid dolla <mmm.lizass@gmail.com>
2025-04-17 22:22:45 +0500

    Добавьте файлы проекта.

---
+++ CirclesForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_laba3_1
+{
+    public partial class CirclesForm : Form
+    {
+        public CirclesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
